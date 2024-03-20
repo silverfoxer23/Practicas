819b3ec1601500093cf048fd4e200ae497f4855f
HoraHena <161860993+horahenaripo@users.noreply.github.com>
2024-03-20 13:44:09 -0500

    yeah

---
+++ JS_Course/SuperSimpleDev/script.js
@@ -0,0 +1,52 @@
+// // parte 1
+// * BASICS
+
+// alert("Hello world");
+
+// alert("horahena")
+
+// console.log(10 + 5);
+
+// console.log(20 - 5);
+
+// console.log(2 + 2 - 5);
+
+// document.body.innerHTML = "Good morning!";
+
+// document.body.appendChild = "horahena"
+
+// // parte 2 
+// * MATH
+
+// console.log(10 + ( 8 * 3 ) + 5 )
+
+// console.log( ( 10 + ( 8 * 3 ) + 5 ) * 3 )
+
+// console.log( (1850 + ( 750 * 200 )) / 1000 );
+
+// console.log( ((1850 + ( 750 * 200 )) / 1000) * 0.10 );
+
+// console.log( ((1850 + ( 750 * 200 )) / 1000) * 0.20 );
+
+// console.log(Math.floor(2.8));
+
+// console.log(Math.ceil(2.2));
+
+// var CEL = 50;
+// var FAR = 50;
+
+// console.log( "50 CEL a FAR: " + ( CEL * 9 / 5 + 32 ) ); 
+// console.log( "50 FAR a CEL: " + ( ( FAR - 32 ) * 5 / 9 ) ); 
+
+// console.log( "25 CEL a FAR: " + ( 25 * 9 / 5 + 32 ) ); 
+// console.log( "86 FAR a CEL: " + ( ( 86 - 32 ) * 5 / 9 ) ); 
+// console.log( "-5 CEL a FAR: " + ( -5 * 9 / 5 + 32 ) ); 
+
+// parte 3 
+// * STRINGS
+
+// var Name = "hora"
+
+// console.log(`My name is ${Name}`);
+
+// alert("Total cost: $" + `${(599 + 395) /100}` + "\n Thank you, come again!");
